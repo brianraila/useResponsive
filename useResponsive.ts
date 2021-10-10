e7a4b39aee29088e30f048b364d04e8978616c09
Amolo <railamolo@gmail.com>
2021-10-10 11:19:32 +0300

    Create useResponsive.ts

---
+++ useResponsive.ts
@@ -0,0 +1,54 @@
+
+import { useEffect, useMemo, useState } from "react";
+
+enum DisplaySize {
+  NotSupported = 0,
+  MobileS = 320,
+  MobileM = 375,
+  MobileL = 425,
+  Tablet = 768,
+  ComputerS = 1024,
+  ComputerM = 1440,
+  ComputerL = 2560,
+}
+
+const determineDisplaySize = (width: number) => {
+  if (width >= DisplaySize.Tablet) {
+    if (width >= DisplaySize.ComputerM) {
+      if (width >= DisplaySize.ComputerL) {
+        return DisplaySize.ComputerL;
+      } else {
+        return DisplaySize.ComputerM;
+      }
+    } else if (width >= DisplaySize.ComputerS) {
+      return DisplaySize.ComputerS;
+    } else {
+      return DisplaySize.Tablet;
+    }
+  } else if (width >= DisplaySize.MobileM) {
+    if (width >= DisplaySize.MobileL) {
+      return DisplaySize.MobileL;
+    } else {
+      return DisplaySize.MobileM;
+    }
+  } else if (width >= DisplaySize.MobileS) {
+    return DisplaySize.MobileS;
+  } else {
+    return DisplaySize.NotSupported;
+  }
+}
+
+
+const useResponsive = () => {
+  const [ currentDisplaySize, setCurrentDisplaySize ] = useState(determineDisplaySize(window.innerWidth));
+
+  useEffect(() => {
+    const handler = () => setCurrentDisplaySize(determineDisplaySize(window.innerWidth));
+    window.addEventListener("resize", handler);
+    return () => window.removeEventListener("resize", handler);
+  }, []);
+
+  return useMemo(() => currentDisplaySize, [currentDisplaySize]);
+}
+
+export default useResponsive;
